ebc47e8b507f26be115546c286896a7c89c9f569
Kevin Wittek <kevin@wittek.dev>
2020-05-14 12:40:05 +0200

    Fix formatting in solidity files

--- truffle/contracts/TokenManager.sol
+++ truffle/contracts/TokenManager.sol
@@ -1,184 +1,159 @@
 pragma solidity ^0.5.16;
 pragma experimental ABIEncoderV2;
-    contract TokenManager{
-        
-        constructor() public {
-            
-        }
+
+
+contract TokenManager {
+    constructor() public {}
 
     //Struct for a Location that wants to give out POV Tokens
-    struct Location{
-        
+    struct Location {
         string tokenname;
         string tokensymbol;
         string locationname;
         address locationaddress;
         uint256 locationID;
-        
     }
-    
 
     //Struct data for all POV-Tokens in existence
-    struct PovToken{
-       
-	  
+    struct PovToken {
         string locationname;
         address locationaddress;
         uint256 locationID;
-  
-	
-
     }
 
+    //Storage for all Locations
+    Location[] public locations;
 
-    //Storage for all Locations 
-     Location[] public locations;
-    
     //Storage for all tokens from all locations in existence
-     PovToken [] public povtokens;
+    PovToken[] public povtokens;
 
+    //hashmaps
 
-   
-   //hashmaps
-      
     //This mapping represents which Index of the povtokens map is owned by who (address)
     //ID is basically the same as the INDEX!! (as far as i understand)
-    mapping (uint256 => address) public tokenIndexToOwner;
-    
+    mapping(uint256 => address) public tokenIndexToOwner;
 
-   
-   //this mapping represents which location an token has
+    //this mapping represents which location an token has
     mapping(uint256 => uint256) public tokenIndexToLocationIndex;
-    
+
     //used to see the total tokens of an address (needed for ERC721)
-    mapping (address => uint256) public ownershipTokenCount;
-    
+    mapping(address => uint256) public ownershipTokenCount;
+
     mapping(uint256 => string) public locationIDtoLocationName;
 
-
-
-//getters and setters for testing
- function getLocationNameFromId(uint256 _id) public view returns  (string memory)
+    //getters and setters for testing
+    function getLocationNameFromId(uint256 _id)
+        public
+        view
+        returns (string memory)
     {
         return locationIDtoLocationName[_id];
     }
 
-  function setLocationNameForId(uint256 _id, string memory _locationname) public 
+    function setLocationNameForId(uint256 _id, string memory _locationname)
+        public
     {
         locationIDtoLocationName[_id] = _locationname;
     }
-   
-   function getLocations() public view returns (Location[] memory)
-   
-   {
-   	return locations;
-   }
-   
-   function returnOneDigitNumers() public pure returns (uint[9] memory)
-   {
-   return [uint(1),2,3,4,5,6,7,8,9];
-   }
-    
 
-//just for testing can be removed later
-function addToken(string memory _locationname, address _locationaddress, uint256 _locationId) public
-{
+    function getLocations() public view returns (Location[] memory) {
+        return locations;
+    }
 
+    function returnOneDigitNumers() public pure returns (uint256[9] memory) {
+        return [uint256(1), 2, 3, 4, 5, 6, 7, 8, 9];
+    }
 
+    //just for testing can be removed later
+    function addToken(
+        string memory _locationname,
+        address _locationaddress,
+        uint256 _locationId
+    ) public {
         PovToken memory _newpovtoken = PovToken({
-       
             locationname: _locationname,
             locationaddress: _locationaddress,
             locationID: _locationId
-            
         });
 
-povtokens.push(_newpovtoken);
-
-}
-
-    
+        povtokens.push(_newpovtoken);
+    }
 
     //creates a new location that can give out tokens to visitors, e.g. "IFIS-Token, Institut für Internetsicherheit"
-    
-    function createLocation(string calldata _tokenname, string calldata _tokensymbol, string calldata _locationname, address _locationWalletAddress ) external returns (uint256)  {
-      
-      //id of the next open Index of LocationArray
-      uint256 _newLocationID = locations.length; 
-      
-         Location memory _newLocation = Location({
-          
-          tokenname: _tokenname,
-          tokensymbol: _tokensymbol,
-          locationname: _locationname,
-          locationaddress: _locationWalletAddress,
-          locationID: _newLocationID
-          
+
+    function createLocation(
+        string calldata _tokenname,
+        string calldata _tokensymbol,
+        string calldata _locationname,
+        address _locationWalletAddress
+    ) external returns (uint256) {
+        //id of the next open Index of LocationArray
+        uint256 _newLocationID = locations.length;
+
+        Location memory _newLocation = Location({
+            tokenname: _tokenname,
+            tokensymbol: _tokensymbol,
+            locationname: _locationname,
+            locationaddress: _locationWalletAddress,
+            locationID: _newLocationID
         });
-        
-        
+
         //adds the location to the location array
         locations.push(_newLocation);
-        
+
         locationIDtoLocationName[_newLocationID] = _locationname;
-        
+
         //returns the locationID for the creator of the location
         return _newLocationID;
-      
-  }
+    }
 
-
-
-  
-  
-  
     //Generates a Token for a Location and sets the requestsaddress as the owner, gets called when Visitor wants a Token from Admin
-  
+
     //locationaddress is currently unused but may be used in later implementations
     //locationId and locationaddress is given by admin
-    
+
     //the function should return the tokenID to Admin to give it to requester. Right know i don't 		//know how to do that
 
-    function requestToken(uint256 _locationID, address _locationaddress, address _requestaddress) external{
-    
- 
+    function requestToken(
+        uint256 _locationID,
+        address _locationaddress,
+        address _requestaddress
+    ) external {
         PovToken memory _newpovtoken = PovToken({
-       
             locationname: locationIDtoLocationName[_locationID],
             locationaddress: _locationaddress,
             locationID: _locationID
-            
         });
-    
-    
+
         //safes token to public manager array
-        uint256 newTokenId = povtokens.push(_newpovtoken) -1;
-    
+        uint256 newTokenId = povtokens.push(_newpovtoken) - 1;
+
         //safes this tokenindex to given locationindex (hashmap)
         tokenIndexToLocationIndex[newTokenId] = _locationID;
-     
+
         //sets requestsaddress as owner of token
         tokenIndexToOwner[newTokenId] = _requestaddress;
-        
+
         //ups the totalBalance of address
         ownershipTokenCount[_requestaddress]++;
-
+    }
 }
-
-
-
-}
-
 
 
 contract ERC721 {
     // Required methods
     function totalSupply() public view returns (uint256 total);
+
     function balanceOf(address _owner) public view returns (uint256 balance);
+
     function ownerOf(uint256 _tokenId) external view returns (address owner);
+
     function approve(address _to, uint256 _tokenId) external;
+
     function transfer(address _to, uint256 _tokenId) external;
-    function transferFrom(address _from, address _to, uint256 _tokenId) external;
+
+    function transferFrom(address _from, address _to, uint256 _tokenId)
+        external;
 
     // Events
     event Transfer(address from, address to, uint256 tokenId);
@@ -191,101 +166,67 @@ contract ERC721 {
     // function tokenMetadata(uint256 _tokenId, string _preferredTransport) public view returns (string infoUrl);
 
     // ERC-165 Compatibility (https://github.com/ethereum/EIPs/issues/165)
-    function supportsInterface(bytes4 _interfaceID) external view returns (bool);
+    function supportsInterface(bytes4 _interfaceID)
+        external
+        view
+        returns (bool);
 }
 
 
-
-
 contract POVToken is TokenManager, ERC721 {
-    
-   
-    
- 
-    
-     function totalSupply() public view returns (uint) {
-        return povtokens.length ;
+    function totalSupply() public view returns (uint256) {
+        return povtokens.length;
     }
-    
-    
-    
-    
-       function totalSupplyLocations() public view returns (uint) {
-        return locations.length ;
+
+    function totalSupplyLocations() public view returns (uint256) {
+        return locations.length;
     }
-    
-  
-    
- 
-    
-    
-  
-     function ownerOf(uint256 _tokenID) external view returns (address owner){
-         
-         return tokenIndexToOwner[_tokenID];
-  
-     }
-     
-  
-   function balanceOf(address _owner) public view returns (uint256 balance)
-   {
-       
-       return ownershipTokenCount[_owner];
-       
-   }
-   
-   
-   //gives the name of the location from given token
-   function locationNameOfToken(uint256 _tokenID) public view returns (string memory 
-locationname)
-   {
-    
-      return locations[tokenIndexToLocationIndex[_tokenID]].locationname;
-  
-   }
-    
-    
-    
-    
-    
-     // |1| Right now there is no plans to send or interchange tokens after the owner is declared. It's self-explanatory because a Token is a Proof of Visit, you can't give your "visit" to someone else
-     
-     //Events
+
+    function ownerOf(uint256 _tokenID) external view returns (address owner) {
+        return tokenIndexToOwner[_tokenID];
+    }
+
+    function balanceOf(address _owner) public view returns (uint256 balance) {
+        return ownershipTokenCount[_owner];
+    }
+
+    //gives the name of the location from given token
+    function locationNameOfToken(uint256 _tokenID)
+        public
+        view
+        returns (string memory locationname)
+    {
+        return locations[tokenIndexToLocationIndex[_tokenID]].locationname;
+    }
+
+    // |1| Right now there is no plans to send or interchange tokens after the owner is declared. It's self-explanatory because a Token is a Proof of Visit, you can't give your "visit" to someone else
+
+    //Events
     event Transfer(address from, address to, uint256 tokenId);
     event Approval(address owner, address approved, uint256 tokenId);
-     
-    //See |1|
-    function  transfer(address requestaddress, uint256 _tokenId) external {
-         
-        
-        emit Transfer(msg.sender, requestaddress, _tokenId);
 
-    }
-     
     //See |1|
-    function approve(address _to, uint256 _tokenId) external
-    {
-        
-       
+    function transfer(address requestaddress, uint256 _tokenId) external {
+        emit Transfer(msg.sender, requestaddress, _tokenId);
+    }
+
+    //See |1|
+    function approve(address _to, uint256 _tokenId) external {
         emit Approval(msg.sender, _to, _tokenId);
     }
-     
-    //See |1|
-    function transferFrom(address _from, address _to, uint256 _tokenId) external
-    {
-          
-       
-        emit Transfer(_from, _to, _tokenId);
-          
-    }
-    
-    
-   function supportsInterface(bytes4 _interfaceID) external view returns (bool){
-        
-       return true;
-   }
-    
-    
-   
 
+    //See |1|
+    function transferFrom(address _from, address _to, uint256 _tokenId)
+        external
+    {
+        emit Transfer(_from, _to, _tokenId);
+    }
+
+    function supportsInterface(bytes4 _interfaceID)
+        external
+        view
+        returns (bool)
+    {
+        return true;
+    }
 }
--- truffle/test/TestTokenManager.sol
+++ truffle/test/TestTokenManager.sol
@@ -1,157 +1,168 @@
 pragma solidity ^0.5.16;
 
-
-
 import "truffle/Assert.sol";
 import "truffle/DeployedAddresses.sol";
 import "..//contracts/TokenManager.sol";
 
-contract TestTokenManager{
 
-
-	 POVToken povtoken;
+contract TestTokenManager {
+    POVToken povtoken;
     string _tokenname;
     string _tokensymbol;
     string _locationname;
-    address _locationWalletAddress ;
-	uint256 _locationId;
-address _requestaddress;
-
-
-
-function beforeEach() public
-{
-	_locationname = "testname";
-    	_locationWalletAddress = 0x793f3338d13D4DB8AC607Efa9101012f3cb3bE5A;
-     	
-	_tokenname = "testtoken";
-	_tokensymbol = "tSY";
-	
-	povtoken = new POVToken();
-	_requestaddress = 0x19d7a2a11b6957912AC232a10a02B5FdD0df9A17;
-
-}
-
-
-
-
-//TypeError: Type struct TokenManager.Location memory[] memory is not implicitly convertible to expected type struct TestTokenManager.Location memory[] memory.
-//Location[] memory locations = povtoken.getLocations();
-//^---------------------------------------------------^
-
-
-function testCreateLocation() public
-{
-
-  povtoken.createLocation(_tokenname, _tokensymbol, _locationname, _locationWalletAddress);
-
-  TokenManager.Location[] memory locations = povtoken.getLocations();
-
-
-
-
-
-}
-
-
-function testTotalSupply() public
-{
-
-uint256 expected = 1;
-
-
-
-povtoken.createLocation(_tokenname, _tokensymbol, _locationname, _locationWalletAddress);
-povtoken.requestToken(0, _locationWalletAddress,_requestaddress);
-
-
-//or
-//povtoken.addToken(_locationname, _locationWalletAddress, _locationId);
-
-  Assert.equal(povtoken.totalSupply(), expected, "It should be 1 token in array ");
-
-
-}
-
-
-uint256 expected1 = 1;
-uint256 expected2 = 0;
-
-function testTotalSupplyLocations() public {
-
-povtoken.createLocation(_tokenname, _tokensymbol, _locationname, _locationWalletAddress);
-
- Assert.equal(povtoken.totalSupplyLocations(), expected1, "It should be 1 location in array ");
- Assert.notEqual(povtoken.totalSupplyLocations(), expected2, "there are no locations but there should be at least 1");
-
-}
-
-
-function testOwnerOf() public
-{
-
-povtoken.createLocation(_tokenname, _tokensymbol, _locationname, _locationWalletAddress);
-povtoken.requestToken(_locationId, _locationWalletAddress,_requestaddress);
-
-
-address expectedaddress = 0x19d7a2a11b6957912AC232a10a02B5FdD0df9A17;
-
-Assert.equal(povtoken.ownerOf(0), expectedaddress, "?");
-
-
-}
-
-function testBalanceOf() public
-{
-
-uint256  expected= 3;
-
-	povtoken.createLocation(_tokenname, _tokensymbol, _locationname, _locationWalletAddress);
-povtoken.requestToken(_locationId, _locationWalletAddress,_requestaddress);
-povtoken.requestToken(_locationId, _locationWalletAddress,_requestaddress);
-povtoken.requestToken(_locationId, _locationWalletAddress,_requestaddress);
-
-Assert.equal(povtoken.balanceOf(_requestaddress), expected, "Should be 3 Tokens ");
-
-
-
-}
-
-function testLocationNameOfToken() public
-{
-povtoken.createLocation(_tokenname, _tokensymbol, _locationname, _locationWalletAddress);
-povtoken.requestToken(0, _locationWalletAddress,_requestaddress);
-povtoken.requestToken(0, _locationWalletAddress,_requestaddress);
-
-
-
- string memory expected = _locationname;
-
-Assert.equal(povtoken.locationNameOfToken(0), expected, "failed");
-Assert.equal(povtoken.locationNameOfToken(1), expected, "failed");
-
-
-
-
-_locationname = "testname2";
-
-
-
-
-povtoken.createLocation(_tokenname, _tokensymbol, _locationname, _locationWalletAddress);
-povtoken.requestToken(1, _locationWalletAddress,_requestaddress);
-
-
-expected = "testname2";
-
-Assert.notEqual(povtoken.locationNameOfToken(0), expected, "failed");
-Assert.notEqual(povtoken.locationNameOfToken(1), expected, "failed");
-Assert.equal(povtoken.locationNameOfToken(2), expected, "failed");
-
-
-
-
-}
-
-
+    address _locationWalletAddress;
+    uint256 _locationId;
+    address _requestaddress;
+
+    function beforeEach() public {
+        _locationname = "testname";
+        _locationWalletAddress = 0x793f3338d13D4DB8AC607Efa9101012f3cb3bE5A;
+
+        _tokenname = "testtoken";
+        _tokensymbol = "tSY";
+
+        povtoken = new POVToken();
+        _requestaddress = 0x19d7a2a11b6957912AC232a10a02B5FdD0df9A17;
+    }
+
+    //TypeError: Type struct TokenManager.Location memory[] memory is not implicitly convertible to expected type struct TestTokenManager.Location memory[] memory.
+    //Location[] memory locations = povtoken.getLocations();
+    //^---------------------------------------------------^
+
+    function testCreateLocation() public {
+        povtoken.createLocation(
+            _tokenname,
+            _tokensymbol,
+            _locationname,
+            _locationWalletAddress
+        );
+
+        TokenManager.Location[] memory locations = povtoken.getLocations();
+    }
+
+    function testTotalSupply() public {
+        uint256 expected = 1;
+
+        povtoken.createLocation(
+            _tokenname,
+            _tokensymbol,
+            _locationname,
+            _locationWalletAddress
+        );
+        povtoken.requestToken(0, _locationWalletAddress, _requestaddress);
+
+        //or
+        //povtoken.addToken(_locationname, _locationWalletAddress, _locationId);
+
+        Assert.equal(
+            povtoken.totalSupply(),
+            expected,
+            "It should be 1 token in array "
+        );
+    }
+
+    uint256 expected1 = 1;
+    uint256 expected2 = 0;
+
+    function testTotalSupplyLocations() public {
+        povtoken.createLocation(
+            _tokenname,
+            _tokensymbol,
+            _locationname,
+            _locationWalletAddress
+        );
+
+        Assert.equal(
+            povtoken.totalSupplyLocations(),
+            expected1,
+            "It should be 1 location in array "
+        );
+        Assert.notEqual(
+            povtoken.totalSupplyLocations(),
+            expected2,
+            "there are no locations but there should be at least 1"
+        );
+    }
+
+    function testOwnerOf() public {
+        povtoken.createLocation(
+            _tokenname,
+            _tokensymbol,
+            _locationname,
+            _locationWalletAddress
+        );
+        povtoken.requestToken(
+            _locationId,
+            _locationWalletAddress,
+            _requestaddress
+        );
+
+        address expectedaddress = 0x19d7a2a11b6957912AC232a10a02B5FdD0df9A17;
+
+        Assert.equal(povtoken.ownerOf(0), expectedaddress, "?");
+    }
+
+    function testBalanceOf() public {
+        uint256 expected = 3;
+
+        povtoken.createLocation(
+            _tokenname,
+            _tokensymbol,
+            _locationname,
+            _locationWalletAddress
+        );
+        povtoken.requestToken(
+            _locationId,
+            _locationWalletAddress,
+            _requestaddress
+        );
+        povtoken.requestToken(
+            _locationId,
+            _locationWalletAddress,
+            _requestaddress
+        );
+        povtoken.requestToken(
+            _locationId,
+            _locationWalletAddress,
+            _requestaddress
+        );
+
+        Assert.equal(
+            povtoken.balanceOf(_requestaddress),
+            expected,
+            "Should be 3 Tokens "
+        );
+    }
+
+    function testLocationNameOfToken() public {
+        povtoken.createLocation(
+            _tokenname,
+            _tokensymbol,
+            _locationname,
+            _locationWalletAddress
+        );
+        povtoken.requestToken(0, _locationWalletAddress, _requestaddress);
+        povtoken.requestToken(0, _locationWalletAddress, _requestaddress);
+
+        string memory expected = _locationname;
+
+        Assert.equal(povtoken.locationNameOfToken(0), expected, "failed");
+        Assert.equal(povtoken.locationNameOfToken(1), expected, "failed");
+
+        _locationname = "testname2";
+
+        povtoken.createLocation(
+            _tokenname,
+            _tokensymbol,
+            _locationname,
+            _locationWalletAddress
+        );
+        povtoken.requestToken(1, _locationWalletAddress, _requestaddress);
+
+        expected = "testname2";
+
+        Assert.notEqual(povtoken.locationNameOfToken(0), expected, "failed");
+        Assert.notEqual(povtoken.locationNameOfToken(1), expected, "failed");
+        Assert.equal(povtoken.locationNameOfToken(2), expected, "failed");
+    }
 }
