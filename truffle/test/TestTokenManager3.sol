83adc2a9eb82323c9107c8c9fc2aecc79fd16321
Marius <marius.weise@web.de>
2020-05-29 16:20:29 +0200

    Test for JsonObject

---
+++ truffle/test/TestTokenManager3.sol
@@ -0,0 +1,67 @@
+pragma solidity ^0.5.16;
+
+import "truffle/Assert.sol";
+import "truffle/DeployedAddresses.sol";
+import "..//contracts/TokenManager.sol";
+
+
+contract TestTokenManager3 {
+    POVToken povtoken;
+
+    // function test() public {
+    //     povtoken = new POVToken();
+
+
+    //         address testlocationaddress
+    //      = 0x793f3338d13D4DB8AC607Efa9101012f3cb3bE5A;
+
+    //     string memory bla = '0x793f3338d13D4DB8AC607Efa9101012f3cb3bE5A';
+
+    //     Assert.equal(bla, povtoken.addressToString(0x793f3338d13D4DB8AC607Efa9101012f3cb3bE5A), "failed");
+    // }
+
+    function testGetUserTokenList() public {
+        povtoken = new POVToken();
+
+
+            address testlocationaddress
+         = 0x793f3338d13D4DB8AC607Efa9101012f3cb3bE5A;
+        string memory testlocationname = "testname";
+        string memory testtokenname = "testtoken";
+        string memory testtokensymbol = "tt";
+
+        setUpLocation(
+            testtokenname,
+            testtokensymbol,
+            testlocationname,
+            testlocationaddress
+        );
+
+        address testUserAddress = 0xe506F9EE73160b3574f6c13942e0616f6096add8;
+        povtoken.requestToken(testlocationaddress, testUserAddress);
+
+
+            string memory expectedJson
+         = '[{"locationaddress":"0x793f3338d13D4DB8AC607Efa9101012f3cb3bE5A","locationname":"testname","tokenname":"testtoken","tokensymbol":"tt","token":0"}]';
+
+        Assert.equal(
+            expectedJson,
+            povtoken.getUserTokenlist(testUserAddress),
+            "failed"
+        );
+    }
+
+    function setUpLocation(
+        string memory testtokenname,
+        string memory testsymbol,
+        string memory testlocationname,
+        address testaddress
+    ) public {
+        povtoken.createLocation(
+            testtokenname,
+            testsymbol,
+            testlocationname,
+            testaddress
+        );
+    }
+}
