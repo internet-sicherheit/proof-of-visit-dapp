499d06258dcf4323a6a86cc476e512889f99b25e
Marius <marius.weise@web.de>
2020-06-04 16:02:48 +0200

    Cleaned up code after fixing Json addressToString problem

--- truffle/test/TestTokenManager3.sol
+++ truffle/test/TestTokenManager3.sol
@@ -8,26 +8,12 @@ import "..//contracts/TokenManager.sol";
 contract TestTokenManager3 {
     POVToken povtoken;
 
-    // function test() public {
-    //     povtoken = new POVToken();
 
-
-    //         address testlocationaddress
-    //      = 0x793f3338d13D4DB8AC607Efa9101012f3cb3bE5A;
-
-    //     string memory bla = '0x793f3338d13D4DB8AC607Efa9101012f3cb3bE5A';
-
-    //     Assert.equal(bla, povtoken.addressToString(0x793f3338d13D4DB8AC607Efa9101012f3cb3bE5A), "failed");
-    // }
 
     function testGetUserTokenList() public {
         povtoken = new POVToken();
-
-
-
-        
-            address testlocationaddress
-         = 0xFDB3D379ab844A402d8D3369077E074318b9Fa01;
+ 
+        address testlocationaddress = 0xFDB3D379ab844A402d8D3369077E074318b9Fa01;
         string memory testlocationname = "testname";
         string memory testtokenname = "testtoken";
         string memory testtokensymbol = "tt";
@@ -43,14 +29,15 @@ contract TestTokenManager3 {
         povtoken.requestToken(testlocationaddress, testUserAddress);
 
 
-            string memory expectedJson
-         ='[{"locationaddress":"0xfdb3d379ab844a402d8d3369077e074318b9fa01","locationname":"testname","tokenname":"testtoken","tokensymbol":"tt","token":0"}]';
+        string memory expectedJson ='[{"locationaddress":"0xfdb3d379ab844a402d8d3369077e074318b9fa01","locationname":"testname","tokenname":"testtoken","tokensymbol":"tt","token":0"}]';
 
-        Assert.equal(
-            expectedJson,
-            povtoken.getUserTokenlist(testUserAddress),
-            "failed"
-        );
+
+
+
+    
+        Assert.equal(expectedJson, povtoken.getUserTokenlist(testUserAddress), "failed");
+
+
 
   // Assert.equal(true,povtoken.compareStrings(expectedJson,povtoken.getUserTokenlist(testUserAddress)),"failed");
 
